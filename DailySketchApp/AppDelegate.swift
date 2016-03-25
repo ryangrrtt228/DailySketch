a4da71dc59706c770897204571eecf8e20a63041
Ryan <ryangrrtt228@gmail.com>
2016-03-25 16:41:59 -0600

    still working on the Update daily feature

--- DailySketchApp/AppDelegate.swift
+++ DailySketchApp/AppDelegate.swift
@@ -19,6 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let notification = NSNotification(name: "CheckLastQuoteRetrieval", object: nil)
+        NSNotificationCenter.defaultCenter().postNotification(notification)
         return true
     }
 
--- DailySketchApp/ViewController.swift
+++ DailySketchApp/ViewController.swift
@@ -17,14 +17,56 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         super.viewDidLoad()
         
         randomWordLabel.text = DailyPrompt.randomWord()
-        
-     
-                    
+    }
+    
+        func checkLastRetrieval() {
+            let drawingPrompt = DailyPrompt.drawingPrompt.count
+            let userDefaults = NSUserDefaults.standardUserDefaults()
+            
+            if let lastRetrieval = userDefaults.dictionaryForKey("lastRetrieval"){
+                if let lastDate = lastRetrieval["Date"] as? NSDate{
+                    if let index = lastRetrieval["index"] as? Int {
+                        if abs(lastDate.timeIntervalSinceNow) > 86400 {
+                            
+                            var nextIndex = index + 1
+                            
+                            if drawingPrompt <= nextIndex {
+                                nextIndex = 0
+                                
+                            }
+                            self.randomWordLabel.text = DailyPrompt.drawingPrompt[nextIndex]
+                            
+                            let lastRetrieval : [NSObject : AnyObject] = [ "date": NSDate(), "index": nextIndex]
+                            userDefaults.setObject(lastRetrieval, forKey: "lastRetrieval")
+                            userDefaults.synchronize()
+                        }
+                    }
+                }
+            } else {
+                self.randomWordLabel.text = DailyPrompt.drawingPrompt.first!
+                
+                let lastRetrieval : [NSObject: AnyObject] = [ "date": NSDate(), "index": 0]
+                
+                userDefaults.setObject(lastRetrieval, forKey: "lastRetrieval")
+                userDefaults.synchronize()
+            }
         }
+        
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.checkLastRetrieval), name: "CheckLastQuoteRetrieval", object: nil)
+        checkLastRetrieval()
+        
+    }
+    
     
     @IBAction func cameraButtonTapped(sender: UIButton) {
         
         self.performSegueWithIdentifier("toImageScreen", sender: nil)
     }
 
+   
 }
