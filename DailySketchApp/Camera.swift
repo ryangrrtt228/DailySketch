d8f5fdd99fcdf50f689b1d92b3689388282a07f6
Ryan <ryangrrtt228@gmail.com>
2016-05-03 16:40:43 -0600

    working With design

---
+++ DailySketchApp/Camera.swift
@@ -0,0 +1,23 @@
+//
+//  Camera.swift
+//  DailySketchApp
+//
+//  Created by Batman on 5/3/16.
+//  Copyright © 2016 Ryan Garrett. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class Camera: UIView {
+
+    
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        CameraKit.drawCamera(frame: self.bounds, resizing: .AspectFit)
+    }
+    
+
+}
